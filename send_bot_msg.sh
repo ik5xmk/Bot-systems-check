fffd24070a69fa851f10e9aa1fb8535b57e1bb1e
David B <ik5xmk@gmail.com>
2022-06-12 15:42:03 +0200

    Update send_bot_msg.sh

--- send_bot_msg.sh
+++ send_bot_msg.sh
@@ -1,2 +1,6 @@
-# set your BOT TOKEN and your CHAT ID
+#!/bin/bash
+
+# set your BOT TOKEN and your CHAT ID number
+# if curl is not present, install as "apt install curl"
+
  curl -s -X POST https://api.telegram.org/bot5594875427:ABHIbYrPlg6bG1D_pxrSqnT01dtCZOGnpIo/sendMessage -d chat_id=-1021453100923 -d text="$1"
